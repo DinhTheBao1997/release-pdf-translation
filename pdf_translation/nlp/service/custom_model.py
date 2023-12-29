1f75e519aa5fd9d4f0ab4a1ea789d3846c6f4cd7
BaoDT <49833157+DinhTheBao1997@users.noreply.github.com>
2023-12-29 18:41:50 +0700

    Auto commit

--- pdf_translation/nlp/service/custom_model.py
+++ pdf_translation/nlp/service/custom_model.py
@@ -1,13 +1,35 @@
 from .translate_core import TransalteCore
+from transformers import MBartForConditionalGeneration, MBartTokenizer
+model_repo = "DinhTheBao1997/mbart-for-medical"
+lang_code = {
+    "tgt": "vi_VN",
+}
+model = MBartForConditionalGeneration.from_pretrained(model_repo)
+tokenizer = MBartTokenizer.from_pretrained(model_repo, tgt_lang=lang_code["tgt"])
 
-def write_file(path, data):
-    with open(path, 'w', encoding='utf-8') as f:
-        f.write(data)
-# from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-# tokenizer_en2vi = AutoTokenizer.from_pretrained("vinai/vinai-translate-en2vi", src_lang="en_XX")
-# model_en2vi = AutoModelForSeq2SeqLM.from_pretrained("vinai/vinai-translate-en2vi")
+split="."
+predictions=[]
+def clean(v: str):
+    return v.strip()
 
+def translate(v: str):
+    if v is None or len(v) == 0:
+        return ""
+    inputs = tokenizer(v, return_tensors="pt")
+    translated_tokens = model.generate(**inputs, decoder_start_token_id=tokenizer.lang_code_to_id[lang_code["tgt"]], early_stopping=True, max_length=1024)
+    pred = tokenizer.batch_decode(translated_tokens, skip_special_tokens=True)[0]
+    return pred
 
+def seperate(para: str):
+    temp=para.split(split)
+    return map(clean, temp)
+
+def decode(lst: list[str]):
+    return ". ".join(lst).strip()
 class CustomModel(TransalteCore):
     def translate_en2vi(raw: str) -> str:
-        return raw
+        sentences = seperate(raw)
+        t_preds = []
+        for sentence in sentences:
+            t_preds.append(translate(sentence))
+        return decode(t_preds)
