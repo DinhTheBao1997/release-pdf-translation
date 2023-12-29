4bfe2201dcdb7739b96948e633df9a56a5629f99
BaoDT <49833157+DinhTheBao1997@users.noreply.github.com>
2023-12-29 19:06:19 +0700

    Auto commit

--- main/settings.py
+++ main/settings.py
@@ -13,11 +13,11 @@ else:
     ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['example.com'])
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 # Databases
-DATABASES = {
-    "default": env.db("DATABASE_URL")
-}
-DATABASES["default"]["ATOMIC_REQUESTS"] = True
-DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)
+# DATABASES = {
+#     "default": env.db("DATABASE_URL")
+# }
+# DATABASES["default"]["ATOMIC_REQUESTS"] = True
+# DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 LOGGING = {
     'version': 1,
