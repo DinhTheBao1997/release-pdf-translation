7df6e12ae84ebb207e73276d3f16ae16ffa205eb
BaoDT <49833157+DinhTheBao1997@users.noreply.github.com>
2023-12-30 20:48:19 +0700

    Auto commit

--- main/settings.py
+++ main/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "pdftranslation.applikuapp.com,127.0.0.1,localhost").split(",")
 
 # Application definition
 
